67135d24f13794137c18c173b2e30c84292dcb3c
Ed Sweeney <ed@onextent.com>
2017-10-15 07:39:43 -0700

    resources

--- src/main/scala/onextent/akka/eventhubs/kafka/Main.scala
+++ src/main/scala/onextent/akka/eventhubs/kafka/Main.scala
@@ -16,6 +16,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App with LazyLogging {
 
+  /*
+
   val conf: Config = ConfigFactory.load()
   val bootstrap: String = conf.getString("kafka.bootstrap")
   val consumerGroup: String = conf.getString("kafka.consumerGroup")
@@ -28,7 +30,6 @@ object Main extends App with LazyLogging {
     .withGroupId(consumerGroup)
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
 
-
   Consumer
     .committableSource(consumerSettings, Subscriptions.topics(topic))
     .mapAsync(10) {
@@ -39,14 +40,14 @@ object Main extends App with LazyLogging {
       }
 
       (msg: ConsumerMessage.CommittableMessage[Array[Byte], String]) =>
-        logger.warn("ejs processing")
         val key = Option(msg.record.key()).getOrElse(msg.hashCode().toString.toArray).toString //todo fix bs
         publish(key, msg.record.value()).map(_ => msg)
     }
     .mapAsync(10) {
       (msg: ConsumerMessage.CommittableMessage[Array[Byte], String]) =>
-        logger.warn("ejs commit")
         msg.committableOffset.commitScaladsl()
     }
     .runWith(Sink.ignore)
+   */
+  ReadKafkaWriteEventhubs()
 }
