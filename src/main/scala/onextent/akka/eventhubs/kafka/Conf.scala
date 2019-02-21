48ae57246825ec00d2869c98772dde88a1f2261c
ingared8 <gangareddy.ts@gmail.com>
2019-02-20 16:45:20 -0800

    Modified kafka setting to read & write to TLS kafka

--- src/main/scala/onextent/akka/eventhubs/kafka/Conf.scala
+++ src/main/scala/onextent/akka/eventhubs/kafka/Conf.scala
@@ -16,12 +16,35 @@ trait Conf {
   val consumerGroup: String = conf.getString("kafka.consumerGroup")
   val topic: String = conf.getString("kafka.topic")
   val parallelism: Int = conf.getInt("kafka.parallelism")
-  val consumerSettings: ConsumerSettings[Array[Byte], String] = ConsumerSettings(actorSystem,
+  val consumerSettingsPlain: ConsumerSettings[Array[Byte], String] = ConsumerSettings(actorSystem,
     new ByteArrayDeserializer,
     new StringDeserializer)
     .withBootstrapServers(bootstrap)
     .withGroupId(consumerGroup)
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+
+  val trustStorePath = conf.getString("trustStorePath")
+  val keyStorePath = conf.getString("keyStorePath")
+  val trustStorePassword =  conf.getString("trustStorePassword")
+  val keyStorePassword =  conf.getString("keyStorePassword")
+  val keyPassword = conf.getString("keyPassword")
+  val securityProtocol = conf.getString("securityProtocol")
+  val saslMechanism = conf.getString("saslMechanism")
+
+  lazy val consumerSettingsWithTLS: ConsumerSettings[Array[Byte], String] =
+    consumerSettingsPlain
+    .withProperty("ssl.truststore.location", trustStorePath)
+    .withProperty("ssl.truststore.password", trustStorePassword)
+      .withProperty("ssl.keystore.location", keyStorePath)
+        .withProperty("ssl.keystore.password", keyStorePassword)
+      .withProperty("ssl.key.password", keyPassword)
+        .withProperty("security.protocol", securityProtocol)
+    .withProperty("sasl.mechanism", saslMechanism)
+
+  val consumerSettings:ConsumerSettings[Array[Byte], String]  = {
+    if (conf.getBoolean("kafka.useTLS")) consumerSettingsWithTLS
+    else consumerSettingsPlain
+  }
 
   val connStr: String = conf.getString("eventhubs.connStr")
 
